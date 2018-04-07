4442aa0c3be2434307643d28d16656c85a4f52e3
skyadamsh <34922053+skyadamsh@users.noreply.github.com>
2018-04-07 16:46:29 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -62,6 +62,8 @@ def authorized():
     else:
         try:
             #save user data and set log in message
+            print(resp['access_token'])
+            print(github.get('user').data)
             session['github_token']=(resp['access_token'],'')
             session['user_data']=github.get('user').data
             message="You were successfully logged in as " + session['user_data']['login']
